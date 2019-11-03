8cd0afc87e1e022f660fe990b512e50eff4b7fd9
Holger Schuh <5843392+nils2000@users.noreply.github.com>
2019-11-03 22:07:16 +0100

    first page inputfield defiened

--- document_script.js
+++ document_script.js
@@ -1,20 +1,24 @@
 'use strict';
-function named_input(name) {
-    var label = document.createElement("label");
+function input_field(name, size) {
     var input = document.createElement("input");
     input.id = name;
+    input.size = size;
     input.name = name;
-    input.size = 2;
+    return input;
+}
+function labeled_input(name) {
+    var label = document.createElement("label");
+    var input = input_field(name, 2);
     label.htmlFor = name;
     label.innerText = name;
     label.appendChild(input);
     return label;
 }
-function named_input_fields(names) {
-    return names.map(named_input);
+function labeled_input_fields(names) {
+    return names.map(labeled_input);
 }
-function append_named_input_field(name, html_element_id) {
-    document.getElementById(html_element_id).appendChild(named_input(name));
+function append_labeled_input_field(name, html_element_id) {
+    document.getElementById(html_element_id).appendChild(labeled_input(name));
 }
 function named_textarea(name, rows) {
     var span = document.createElement("span");
@@ -28,6 +32,32 @@ function named_textarea(name, rows) {
 }
 function append_named_textarea(name, rows, html_element_id) {
     document.getElementById(html_element_id).appendChild(named_textarea(name, rows));
+}
+function named_input_field(name, size) {
+    var input = input_field(name, size);
+    input.value = name;
+    var span = document.createElement("span");
+    span.title = name;
+    span.appendChild(input);
+    return span;
+}
+function weapon_stats_inputfields() {
+    var weapon_num = Date.now();
+    var weapon_div = document.createElement("div");
+    weapon_div.id = weapon_num;
+    var weapon_name_field = named_input_field("Waffe", 15);
+    weapon_name_field.className = "weapon_name";
+    var weapon_bonus_field = named_input_field("Bonus", 5);
+    weapon_bonus_field.className = "weapon_bonus";
+    var weapon_damage_field = named_input_field("Schaden/Art", 15);
+    weapon_damage_field.className = "weapon_damage";
+    weapon_div.appendChild(weapon_name_field);
+    weapon_div.appendChild(weapon_bonus_field);
+    weapon_div.appendChild(weapon_damage_field);
+    return weapon_div;
+}
+function add_new_weapon() {
+    document.getElementById("weapons").appendChild(weapon_stats_inputfields());
 }
 function add_skills() {
     var skills = [
@@ -50,7 +80,7 @@ function add_skills() {
         "Täuschen(Cha)",
         "Überzeugen(Cha)"
     ];
-    var inputs = named_input_fields(skills);
+    var inputs = labeled_input_fields(skills);
     var skill_div = document.getElementById("skills");
     for (var i = 0; i < inputs.length; i++) {
         skill_div.appendChild(inputs[i]);
@@ -58,16 +88,24 @@ function add_skills() {
     }
 }
 add_skills();
+append_labeled_input_field("passive Weisheit (Wahrnehmung)", "passive_wisdom");
 append_named_textarea("weitere Übung und Sprachen", 12, "practice_and_languages");
 append_named_textarea("Persönlichkeitsmerkmale", 5, "personality");
 append_named_textarea("Ideale", 5, "ideals");
 append_named_textarea("Bindungen", 5, "ties");
 append_named_textarea("Makel", 5, "taint");
 append_named_textarea("Merkmale", 40, "characteristics");
-append_named_input_field("RK", "combat_stats");
-append_named_input_field("Initiative", "combat_stats");
-append_named_input_field("Bewegung", "combat_stats");
+append_labeled_input_field("RK", "combat_stats");
+append_labeled_input_field("Initiative", "combat_stats");
+append_labeled_input_field("Bewegung", "combat_stats");
 document.getElementById("combat_stats").appendChild(document.createElement("br"));
-append_named_input_field("TP max", "combat_stats");
-append_named_input_field("TP", "combat_stats");
-append_named_input_field("TP temp", "combat_stats");
+append_labeled_input_field("TP max", "combat_stats");
+append_labeled_input_field("TP", "combat_stats");
+append_labeled_input_field("TP temp", "combat_stats");
+append_labeled_input_field("KM", "money");
+append_labeled_input_field("SM", "money");
+append_labeled_input_field("EM", "money");
+document.getElementById("money").appendChild(document.createElement("br"));
+append_labeled_input_field("GM", "money");
+append_labeled_input_field("PM", "money");
+append_named_textarea("Ausrüstung", 30, "equipment");
--- document_script.ts
+++ document_script.ts
@@ -1,23 +1,28 @@
 'use strict';
 
-function named_input(name: string) {
-    let label = document.createElement("label");
+function input_field(name: string, size: number) {
     let input = document.createElement("input");
     input.id = name;
+    input.size = size;
     input.name = name;
-    input.size = 2;
+    return input;
+}
+
+function labeled_input(name: string) {
+    let label = document.createElement("label");
+    let input = input_field(name, 2);
     label.htmlFor = name;
     label.innerText = name;
     label.appendChild(input);
     return label;
 }
 
-function named_input_fields(names: string[]) {
-    return names.map(named_input);
+function labeled_input_fields(names: string[]) {
+    return names.map(labeled_input);
 }
 
-function append_named_input_field(name: string, html_element_id: string) {
-    document.getElementById(html_element_id).appendChild(named_input(name));
+function append_labeled_input_field(name: string, html_element_id: string) {
+    document.getElementById(html_element_id).appendChild(labeled_input(name));
 }
 
 function named_textarea(name: string, rows: number) {
@@ -34,6 +39,35 @@ function named_textarea(name: string, rows: number) {
 
 function append_named_textarea(name: string, rows: number, html_element_id: string) {
     document.getElementById(html_element_id).appendChild(named_textarea(name, rows));
+}
+
+function named_input_field(name: string, size: number) {
+    let input = input_field(name, size);
+    input.value = name;
+    let span = document.createElement("span");
+    span.title = name;
+    span.appendChild(input);
+    return span;
+}
+
+function weapon_stats_inputfields() {
+    let weapon_num = Date.now();
+    let weapon_div: any = document.createElement("div");
+    weapon_div.id = weapon_num;
+    let weapon_name_field = named_input_field("Waffe", 15);
+    weapon_name_field.className = "weapon_name";
+    let weapon_bonus_field = named_input_field("Bonus", 5);
+    weapon_bonus_field.className = "weapon_bonus";
+    let weapon_damage_field = named_input_field("Schaden/Art", 15);
+    weapon_damage_field.className = "weapon_damage";
+    weapon_div.appendChild(weapon_name_field);
+    weapon_div.appendChild(weapon_bonus_field);
+    weapon_div.appendChild(weapon_damage_field);
+    return weapon_div;
+}
+
+function add_new_weapon() {
+    document.getElementById("weapons").appendChild(weapon_stats_inputfields());
 }
 
 function add_skills() {
@@ -57,7 +91,7 @@ function add_skills() {
         "Täuschen(Cha)",
         "Überzeugen(Cha)"
     ];
-    let inputs = named_input_fields(skills);
+    let inputs = labeled_input_fields(skills);
     let skill_div = document.getElementById("skills");
     for (let i = 0; i < inputs.length; i++) {
         skill_div.appendChild(inputs[i]);
@@ -66,6 +100,9 @@ function add_skills() {
 }
 
 add_skills();
+
+append_labeled_input_field("passive Weisheit (Wahrnehmung)", "passive_wisdom");
+
 append_named_textarea("weitere Übung und Sprachen", 12, "practice_and_languages");
 append_named_textarea("Persönlichkeitsmerkmale", 5, "personality");
 append_named_textarea("Ideale", 5, "ideals");
@@ -73,10 +110,20 @@ append_named_textarea("Bindungen", 5, "ties");
 append_named_textarea("Makel", 5, "taint");
 append_named_textarea("Merkmale", 40, "characteristics");
 
-append_named_input_field("RK", "combat_stats");
-append_named_input_field("Initiative", "combat_stats");
-append_named_input_field("Bewegung", "combat_stats");
+
+append_labeled_input_field("RK", "combat_stats");
+append_labeled_input_field("Initiative", "combat_stats");
+append_labeled_input_field("Bewegung", "combat_stats");
 document.getElementById("combat_stats").appendChild(document.createElement("br"));
-append_named_input_field("TP max", "combat_stats");
-append_named_input_field("TP", "combat_stats");
-append_named_input_field("TP temp", "combat_stats");
+append_labeled_input_field("TP max", "combat_stats");
+append_labeled_input_field("TP", "combat_stats");
+append_labeled_input_field("TP temp", "combat_stats");
+
+append_labeled_input_field("KM", "money");
+append_labeled_input_field("SM", "money");
+append_labeled_input_field("EM", "money");
+document.getElementById("money").appendChild(document.createElement("br"));
+append_labeled_input_field("GM", "money");
+append_labeled_input_field("PM", "money");
+
+append_named_textarea("Ausrüstung", 30, "equipment");
